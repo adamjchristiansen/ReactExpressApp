5a07ed60a3caf17f1d4c0f0e6515bff101b60db5
Adam Christiansen <adam.j.christiansen@gmail.com>
2016-03-18 00:24:49 -0600

    through creating a react component but something is broken and not enough time to fix it right now. Might be from a section or two ago

--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,7 @@
+var React = require('react/addons');
+
 console.log("hello from JSX!");
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+React.render(<GroceryItemList />, app); //app = id of div tag
